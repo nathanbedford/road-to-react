ea11138d13b2f03f3f48e556ca633cb8416323bc
Nathan Bedford <nathan.bedford@gmail.com>
2017-11-23 00:28:57 -0500

    Started with API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const DEFAULT_QUERY = 'redux';
+const DEFAULT_PAGE = 0;
+const DEFAULT_HPP = '100';
+const PATH_BASE = 'https://hn.algolia.com/api/v1'
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
+const PARAM_PAGE = 'page=';
+const PARAM_HPP = 'hitsPerPage=';
+
 const list = [
   {
     title: 'React',
@@ -28,19 +37,56 @@ class App extends Component {
     super(props);
 
     this.state = {
-      firstName: 'Nate',
-      list,
-      searchTerm: ''
+      result: null,
+      searchTerm: DEFAULT_QUERY
     }
 
-    this.onDimiss = this.onDimiss.bind(this);
+    this.setSearchTopstories = this.setSearchTopstories.bind(this);
+    this.fetchSearchTopstories = this.fetchSearchTopstories.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
+    this.onSearchSubmit = this.onSearchSubmit.bind(this);
+    this.onDismiss = this.onDismiss.bind(this);
   }
 
-  onDimiss(id) {
+  setSearchTopstories(result) {
+    const {hits, page} = result;
+
+    const oldHits = page !== 0 ? this.state.result.hits : [];
+    const updatedHits = [...oldHits, ...hits];
+
+    this.setState({
+      result: {hits: updatedHits, page}
+    });
+  }
+
+  fetchSearchTopstories(searchTerm, page) {
+    const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`;
+
+    console.log(url);
+
+    fetch(url)
+    .then(response => response.json())
+    .then(result => this.setSearchTopstories(result))
+    .catch(e => e);
+  }
+
+  componentDidMount() {
+    const { searchTerm } = this.state;
+    this.fetchSearchTopstories(searchTerm, DEFAULT_PAGE);
+  }
+
+  onSearchSubmit(event) {
+    const { searchTerm } = this.state;
+    this.fetchSearchTopstories(searchTerm, DEFAULT_PAGE);
+    event.preventDefault();
+  }
+
+  onDismiss(id) {
     const isNotId = item => item.objectID !== id;
-    const updatedList = this.state.list.filter(isNotId);
-    this.setState({ list: updatedList });
+    const updatedList = this.state.result.hits.filter(isNotId);
+    this.setState({
+      result: { ...this.state.result, hits: updatedList}
+    });
   }
 
   onSearchChange(event) {
@@ -48,23 +94,30 @@ class App extends Component {
   }
 
   render() {
-
-    const { searchTerm, list } = this.state;
-
+    const { searchTerm, result } = this.state;
+    const page = (result && result.page) || 0;
     return (
       <div className="page">
         <div className="interactions">
           <Search
             value={searchTerm}
             onChange={this.onSearchChange}
+            onSubmit={this.onSearchSubmit}
           >Search</Search>
         </div>
         <div>
+          {
+            result &&
           <Table
-            list={list}
-            pattern={searchTerm}
+            list={result.hits}
             onDismiss={this.onDismiss}
           />
+          }
+          <div className="interactions">
+            <Button onClick={() => this.fetchSearchTopstories(searchTerm, page + 1)}>
+            More
+            </Button>
+          </div>
         </div>
       </div>
     );
@@ -74,13 +127,21 @@ class App extends Component {
 export default App;
 
 
-const Search = ({ value, onChange, children }) =>
-  <form>
+const Search = ({
+  value,
+  onChange,
+  onSubmit,
+  children
+}) =>
+  <form onSubmit={onSubmit}>
     {children} <input
       type="text"
       value={value}
       onChange={onChange}
     />
+    <button type="submit">
+      {children}
+    </button>
   </form>
 
 const largeColumn = {
@@ -95,9 +156,9 @@ const smallColumn = {
 
 
 
-const Table = ({ list, pattern, onDismiss }) =>
+const Table = ({ list, onDismiss }) =>
   <div className="table">
-    {list.filter(isSearched(pattern)).map(item =>
+    {list.map(item =>
       <div key={item.objectID} className="table-row">
         <span style={largeColumn}>
           <a href={item.url}>{item.title}</a>
